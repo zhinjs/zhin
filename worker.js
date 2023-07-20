e657b1e0661a698f08b5ec996781ff65dc801c07
Viki <hi@viki.moe>
2023-07-20 16:47:13 +0800

    chore: lint code using prettier

--- worker.js
+++ worker.js
@@ -1,12 +1,10 @@
-const path=require('path')
-let {configPath='zhin.yaml',mode='',entry}=process.env
-process.on('unhandledRejection',(e)=>{
-    console.error(e)
-})
-process.on('uncaughtException',(e)=>{
-    console.error(e)
-})
-entry=path.resolve(__dirname,entry||'lib')
-require(entry)
-    .createZhin(path.resolve(process.cwd(),configPath))
-    .start(mode)
+const path = require("path");
+let { configPath = "zhin.yaml", mode = "", entry } = process.env;
+process.on("unhandledRejection", e => {
+    console.error(e);
+});
+process.on("uncaughtException", e => {
+    console.error(e);
+});
+entry = path.resolve(__dirname, entry || "lib");
+require(entry).createZhin(path.resolve(process.cwd(), configPath)).start(mode);
