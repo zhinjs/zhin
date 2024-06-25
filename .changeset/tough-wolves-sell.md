389c761c44ff89c26830bbf3a224d1f8ca2690a1
凉菜 <1659488338@qq.com>
2024-06-25 14:50:02 +0800

    docs(changeset): fix: use leveldb as built-in db

---
+++ .changeset/tough-wolves-sell.md
@@ -0,0 +1,11 @@
+---
+"@zhinjs/plugin-group-manage": patch
+"@zhinjs/plugin-screenshot": patch
+"@zhinjs/plugin-schedule": patch
+"@zhinjs/plugin-qa": patch
+"@zhinjs/core": patch
+"test": patch
+"zhin": patch
+---
+
+fix: use leveldb as built-in db
---
+++ test/plugins/components/vTable.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+type Header = {
+  label: string;
+  prop: string;
+};
+defineProps<{
+  headers?: Header[];
+  data: Record<string, any>[];
+}>();
+</script>
+
+<template>
+  <table>
+    <tr v-if="headers?.length">
+      <th v-for="header in headers">{{ header.label }}</th>
+    </tr>
+    <tr v-for="item in data">
+      <template v-if="headers?.length">
+        <td v-for="header in headers">
+          {{ item[header.prop] }}
+        </td>
+      </template>
+      <template v-else>
+        <td v-for="value in item">{{ value }}</td>
+      </template>
+    </tr>
+  </table>
+</template>
+
+<style scoped lang="less"></style>
