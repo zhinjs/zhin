fc1b96d8e23e026760e32b6a251437353b1e1fa6
Class Liu <chunlang.liu@rdc-west.com>
2025-10-14 17:48:59 +0800

    fix: 精简代码

--- test-bot/src/plugins/music.tsx
+++ test-bot/src/plugins/music.tsx
@@ -1,6 +1,6 @@
-import { addCommand,MessageCommand, useContext } from "zhin.js";
+import { addCommand, MessageCommand, useContext } from "zhin.js";
 
-async function musicQQ(keyword:string) {
+async function musicQQ(keyword: string): Promise<{ type: 'qq', id: string }[]> {
     const url = new URL('https://c.y.qq.com/splcloud/fcgi-bin/smartbox_new.fcg');
     url.searchParams.set('key', keyword);
     url.searchParams.set('format', 'json');
@@ -8,60 +8,40 @@ async function musicQQ(keyword:string) {
         data
     } = await fetch(url, {
         method: 'GET',
-    }).then(res => res.json() as Promise<{data: {song: {itemlist: {id: string}[]}}}>);
-    console.log('qq',data)
+    }).then(res => res.json() as Promise<{ data: { song: { itemlist: { id: string }[] } } }>);
     return data.song.itemlist.map((song) => ({
         type: 'qq',
         id: song.id,
-    }))[0]
+    }))
 }
-async function music163(keyword:string) {
+async function music163(keyword: string): Promise<{ type: '163', id: string }[]> {
     const url = new URL('https://music.163.com/api/search/get/');
-    const result=await fetch(url, {
+    const result = await fetch(url, {
         method: 'post',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
         },
-        body: new URLSearchParams({
-            s: keyword,
-            type:'1',
-            limit:'1',
-            offset:'0',
-        }),
-    }).then(res => res.json() as Promise<{result: {songs: {id: string}[]}}>)
-    console.log(163,result)
-    return {type: "163", id: result.result.songs[0].id};
+        body: new URLSearchParams({ s: keyword, type: '1', limit: '1', offset: '0'}),
+    }).then(res => res.json() as Promise<{ result: { songs: { id: string }[] } }>)
+    return result.result.songs.map((song) => ({
+        type: '163',
+        id: song.id,
+    }))
 }
-useContext('icqq',(adapter)=>{
+useContext('icqq', (adapter) => {
     addCommand(new MessageCommand('点歌<keyword>')
-        .action(async (message,result)=>{
+        .action(async (message, result) => {
             const keyword = result.params.keyword
-            const music = await music163(keyword)
-            const bot=adapter.bots.get(message.$bot)
-            if(bot){
-                switch(message.$channel.type){
+            const [[musicFromQQ], [musicFrom163]] = await Promise.all([musicQQ(keyword), music163(keyword)])
+            const needSendList = [musicFromQQ, musicFrom163].filter(Boolean)
+            const bot = adapter.bots.get(message.$bot)
+            if (bot) {
+                switch (message.$channel.type) {
                     case 'private':
-                        await bot.pickFriend(+message.$channel.id).shareMusic('163',music.id)
+                        await Promise.all(needSendList.map(music => bot.pickFriend(+message.$channel.id).shareMusic(music.type, music.id)))
                         break
                     case 'group':
-                        await bot.pickGroup(+message.$channel.id).shareMusic('163',music.id)
-                        break
-                }
-            }
-        })
-    )
-    addCommand(new MessageCommand('点歌<keyword>')
-        .action(async (message,result)=>{
-            const keyword = result.params.keyword
-            const music = await musicQQ(keyword)
-            const bot=adapter.bots.get(message.$bot)
-            if(bot){
-                switch(message.$channel.type){
-                    case 'private':
-                        await bot.pickFriend(+message.$channel.id).shareMusic('qq',music.id)
-                        break
-                    case 'group':
-                        await bot.pickGroup(+message.$channel.id).shareMusic('qq',music.id)
+                        await Promise.all(needSendList.map(music => bot.pickGroup(+message.$channel.id).shareMusic(music.type, music.id)))
                         break
                 }
             }
