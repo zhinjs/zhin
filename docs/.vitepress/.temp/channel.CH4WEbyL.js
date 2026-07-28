61a298a1595bb2aa8469d06f5e0d7d9c5ec7326b
moy <mmoy7235@gmail.com>
2026-07-28 10:20:19 +0800

    chore: 移除误提交的 vitepress .temp 构建产物并加入 gitignore

--- docs/.vitepress/.temp/channel.CH4WEbyL.js
+++
@@ -1,7 +0,0 @@
-import { U as Utils, E as Color } from "./app.js";
-const channel = (color, channel2) => {
-  return Utils.lang.round(Color.parse(color)[channel2]);
-};
-export {
-  channel as c
-};
--- docs/.vitepress/.temp/virtual_mermaid-config.C860X2DQ.js
+++
@@ -1,4 +0,0 @@
-const _virtual_mermaidConfig = { "securityLevel": "loose", "startOnLoad": true, "flowchart": { "useMaxWidth": true, "htmlLabels": true } };
-export {
-  _virtual_mermaidConfig as default
-};
